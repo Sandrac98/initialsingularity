e5a130c146985d330e627a8ec7caeb025713b208
Sandra C <114778027+Sandrac98@users.noreply.github.com>
2023-09-20 19:59:09 +0000

    Update context processor

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib import messages
+from django.shortcuts import render, redirect
+
+from products.models import Product
 
 # Create your views here.
 
@@ -12,7 +13,7 @@ def view_shopping_bag(request):
 def add_to_shopping_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
-    quantity = int(request.POST.get('quantity', 1))
+    quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     shopping_bag = request.session.get('shopping_bag', {})
 
@@ -22,5 +23,4 @@ def add_to_shopping_bag(request, item_id):
         shopping_bag[item_id] = quantity
 
     request.session['shopping_bag'] = shopping_bag
-    print(request.session['shopping_bag'])
     return redirect(redirect_url)

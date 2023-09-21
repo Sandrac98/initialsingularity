3ff700e9d9c7b8e169abdc1fcfb2e486b7b4d7e6
Sandra C <114778027+Sandrac98@users.noreply.github.com>
2023-09-21 11:27:23 +0000

    Add update/removefrom shopping bag views and calc subtotal template

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import HttpResponse
+from django.views.decorators.http import require_POST
+from products.models import Product
+from django.contrib import messages
 
 
 # Create your views here.
@@ -23,3 +27,42 @@ def add_to_shopping_bag(request, item_id):
 
     request.session['shopping_bag'] = shopping_bag
     return redirect(redirect_url)
+
+
+def update_shopping_bag(request, item_id):
+    """ Adjust the quantity of the specified product on the shopping bag """
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity', 1))
+    shopping_bag = request.session.get('shopping_bag', {})
+
+    if quantity > 0:
+        shopping_bag[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {shopping_bag[item_id]}')   # noqa
+    else:
+        shopping_bag.pop(item_id, None)
+
+    request.session['shopping_bag'] = shopping_bag
+    return redirect(reverse('view_shopping_bag'))
+
+
+@require_POST
+def remove_from_shopping_bag(request, item_id):
+    """ Remove the specified product from the shopping bag """
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        shopping_bag = request.session.get('shopping_bag', {})
+
+        if item_id in shopping_bag:
+            shopping_bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag')
+
+        else:
+            pass
+
+        request.session['shopping_bag'] = shopping_bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)

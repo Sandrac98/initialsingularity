c140cde7982025ee3a62388db960904fd83d4e0a
Sandra C <114778027+Sandrac98@users.noreply.github.com>
2023-09-24 01:18:41 +0000

    Add checkout template

--- shopping_bag/urls.py
+++ shopping_bag/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('shopping_bag/', views.view_shopping_bag, name='view_shopping_bag'),
+    path('', views.view_shopping_bag, name='view_shopping_bag'),
     path('add/<item_id>/', views.add_to_shopping_bag, name='add_to_shopping_bag'),  # noqa
     path('update/<item_id>/', views.update_shopping_bag, name='update_shopping_bag'),  # noqa
     path('remove/<item_id>/', views.remove_from_shopping_bag, name='remove_from_shopping_bag'),  # noqa

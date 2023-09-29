b3a7aaf9b4ede1e8d9b03cf717c636f9a0ab5e68
Sandra C <114778027+Sandrac98@users.noreply.github.com>
2023-09-29 21:15:58 +0000

    Update AWS config

--- initialsingularity/settings.py
+++ initialsingularity/settings.py
@@ -174,6 +174,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+AWS = True
 
 if 'USE_AWS' in os.environ:
     # Cache control
